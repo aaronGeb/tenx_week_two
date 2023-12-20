68f5c5e916a84f647c963b676ef1cb7d1d89fa9d
Aaron Gebremaraim <mulabr673@gmail.com>
2023-12-20 20:01:12 +0300

    add dbt file

--- scripts/.ipynb_checkpoints/logger-checkpoint.py
+++ scripts/.ipynb_checkpoints/logger-checkpoint.py
@@ -1,14 +1,12 @@
-
 import logging
 import os
-
 
 
 class Logger:
     """Logger class for logging messages to a file."""
 
-    def __init__(self, file_name: str='logs.log', level=logging.INFO):
-        """Initialize logger class with file name to be written and default log level.
+    def __init__(self, file_name: str = "logs.log", level=logging.INFO):
+        """Initilize logger class with file name to be written and default log level.
         Args:
             file_name(str): _description_
             basic_level(_type_, optional): _description_. Defaults to logging.INFO.
@@ -18,14 +16,19 @@ class Logger:
         # Check whether the specified path exists or not
         isExist = os.path.exists(path)
 
-        # Create a new directory because it does not exist 
+        # Create a new directory because it does not exist
         if not isExist:
             os.makedirs(path)
 
         #  # Gets or creates a logger
         logger = logging.getLogger(__name__)
 
-        logging.basicConfig(filename=f'../logs/{file_name}', encoding='utf-8', level=level,format='%(asctime)s - %(name)s - %(levelname)s(message)s')
+        logging.basicConfig(
+            filename=f"../logs/{file_name}",
+            encoding="utf-8",
+            level=level,
+            format="%(asctime)s - %(name)s - %(levelname)s(message)s",
+        )
 
         self.logger = logger
 
